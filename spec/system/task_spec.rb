4994732469537f93d83203e7d24360b25f7ac972
tomoo sasaabe <sasa.sakurajima@gmail.com>
2020-09-14 12:59:18 +0900

    タイトルとステータス検索のテスト完了

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -65,4 +65,42 @@ describe 'タスク管理機能', type: :system do
        end
      end
   end
+  describe '検索機能' do
+    before do
+      # 必要に応じて、テストデータの内容を変更して構わない
+      FactoryBot.create(:task_first, name: "task1", status: "未着手")
+      FactoryBot.create(:task_second, name: "task2", status: "完了")
+      FactoryBot.create(:task_second, name: "sample", status: "完了")
+    end
+    context 'タイトルであいまい検索をした場合' do
+      it "検索キーワードを含むタスクで絞り込まれる" do
+        visit tasks_path
+        # タスクの検索欄に検索ワードを入力する (例: task)
+        fill_in 'search1', with: 'ta'
+        # 検索ボタンを押す
+        click_on '検索'
+        expect(page).to have_content 'task1'
+      end
+    end
+    context 'ステータス検索をした場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        visit tasks_path
+        # プルダウンを選択する「select」について調べてみること
+        select '未着手', from: 'search2'
+        click_on '検索'
+        expect(page).to have_content 'task1'
+        expect(page).to have_no_content 'sample'
+      end
+    end
+    context 'タイトルのあいまい検索とステータス検索をした場合' do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+        visit tasks_path
+        fill_in 'search1', with: 'ta'
+        select '完了', from: 'search2'
+        click_on '検索'
+        expect(page).to have_no_content 'task1'
+        expect(page).to have_content 'task2'
+      end
+    end
+  end
 end
