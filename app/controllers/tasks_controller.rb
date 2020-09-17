d3d85742cc5a9fe533efbb72b0dd3af0a1685394
tomoo sasaabe <sasa.sakurajima@gmail.com>
2020-09-17 09:03:31 +0900

    「自分が作成したタスクだけを表示させる」を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,6 +7,7 @@ class TasksController < ApplicationController
   end
   def create
     @task = Task.new(task_params)
+    @task.user_id = current_user.id
     if @task.save
       redirect_to task_path(@task.id), notice: "登録しました"
     else
@@ -17,23 +18,41 @@ class TasksController < ApplicationController
   def edit
   end
 
-  def index
-    if params[:sort_expired]
-      @tasks = Task.all.order(expired_at: :desc)
-    elsif params[:sort_expired2]
-      @tasks = Task.all.order(priority: :asc)
-    else
-      @tasks = Task.all.order(created_at: :desc)
-      if params[:search1].present? && params[:search2].present?
-        @tasks = @tasks.where('name LIKE ?', "%#{params[:search1]}%").where(status: params[:search2])
-      else
-        @tasks = @tasks.where('name LIKE ?', "%#{params[:search1]}%") if params[:search1].present?
-        @tasks = @tasks.where(status: params[:search2]) if params[:search2].present?
-      end
-    end
-    @tasks = @tasks.page(params[:page]).per(PER)
-  end
+  # def index
+  #   binding.irb
+  #   @tasks = Task.all.order(created_at: :desc)
+  #   @tasks = @tasks.where(user_id: current_user)
+  #   if params[:sort_expired]
+  #     @tasks = @tasks.order(expired_at: :desc)
+  #   elsif params[:sort_priority]
+  #     @tasks = @tasks.order(priority: :asc)
+  #   else
+  #     if params[:search1].present? && params[:search2].present?
+  #       @tasks = @tasks.where('name LIKE ?', "%#{params[:search1]}%")
+  #     else
+  #       @tasks = @tasks.where('name LIKE ?', "%#{params[:search1]}%") if params[:search1].present?
+  #       @tasks = @tasks.where(status: params[:search2]) if params[:search2].present?
+  #     end
+  #   end
+  #   @tasks = @tasks.page(params[:page]).per(PER)
+  # end
 
+  def index
+      if params[:sort_expired]
+        @tasks = Task.all.order(expired_at: :desc).where(user_id:current_user)
+      elsif params[:sort_priority]
+        @tasks = Task.all.order(priority: :asc).where(user_id:current_user)
+      else
+        @tasks = Task.all.order(created_at: :desc).where(user_id:current_user)
+        if params[:search1].present? && params[:search2].present?
+          @tasks = @tasks.where('name LIKE ?', "%#{params[:search1]}%").where(user_id:current_user)
+        else
+          @tasks = @tasks.where('name LIKE ?', "%#{params[:search1]}%").where(user_id:current_user) if params[:search1].present?
+          @tasks = @tasks.where(status: params[:search2]).where(user_id:current_user) if params[:search2].present?
+        end
+      end
+      @tasks = @tasks.page(params[:page]).per(PER)
+  end
 
 
   def show
@@ -52,7 +71,7 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:name, :content, :expired_at, :status, :priority)
+    params.require(:task).permit(:name, :content, :expired_at, :status, :priority, :user_id)
   end
   def set_task
     @task = Task.find(params[:id])
